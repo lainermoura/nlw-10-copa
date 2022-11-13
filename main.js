6294d973df4ec9247ba4a2b67994949e8c37d9ee
Laine Moura <lainermoura@gmail.com>
2022-11-13 17:33:32 -0300

    change main color: remove icon name to mobile

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 let createGame = (player1, hour, player2) => {
     return `
     <li>
-        <div>
-            <img src="./assets/icon-${player1}.svg" alt="icon-${player1}-flag" title="${player1.toUpperCase().replace('-', ' ')}">
-            <p>${player1.toUpperCase().replace('-', ' ')}</p>
-        </div>
+        <img src="./assets/icon-${player1}.svg" alt="icon-${player1}-flag" title="${player1.toUpperCase().replace('-', ' ')}">
         <strong>${hour}</strong>
-        <div>
-            <img src="./assets/icon-${player2}.svg" alt="icon-${player2}-flag" title="${player2.toUpperCase().replace('-', ' ')}">
-            <p>${player2.toUpperCase().replace('-', ' ')}</p>
-        </div>
+        <img src="./assets/icon-${player2}.svg" alt="icon-${player2}-flag" title="${player2.toUpperCase().replace('-', ' ')}">
     </li>
 `
 }
@@ -46,25 +40,25 @@ document.querySelector('#cards').innerHTML =
         createGame('spain', '13:00', 'costa-rica') +
         createGame('belgium', '16:00', 'canada')) +
     createCard(
-        '24/11', 'quinta', 
+        '24/11', 'quinta',
         createGame('switzerland', '07:00', 'cameroon') +
         createGame('uruguay', '10:00', 'south-korea') +
         createGame('portugal', '13:00', 'ghana') +
         createGame('brazil', '16:00', 'serbia')) +
     createCard(
-        '25/11', 'sexta', 
+        '25/11', 'sexta',
         createGame('wales', '07:00', 'iran') +
         createGame('qatar', '10:00', 'senegal') +
         createGame('netherlands', '13:00', 'ecuador') +
         createGame('england', '16:00', 'usa')) +
     createCard(
-        '26/11', 'sábado', 
+        '26/11', 'sábado',
         createGame('tunisia', '07:00', 'australia') +
         createGame('poland', '10:00', 'saudi-arabia') +
         createGame('france', '13:00', 'denmark') +
         createGame('argentina', '16:00', 'mexico')) +
-     createCard(
-        '27/11', 'domingo', 
+    createCard(
+        '27/11', 'domingo',
         createGame('japan', '07:00', 'costa-rica') +
         createGame('belgium', '10:00', 'morocco') +
         createGame('croatia', '13:00', 'canada') +
@@ -88,13 +82,13 @@ document.querySelector('#cards').innerHTML =
         createGame('poland', '16:00', 'argentina') +
         createGame('saudi-arabia', '16:00', 'mexico')) +
     createCard(
-        '01/12', 'quinta', 
+        '01/12', 'quinta',
         createGame('croatia', '12:00', 'belgium') +
         createGame('canada', '12:00', 'morocco') +
         createGame('japan', '16:00', 'spain') +
         createGame('costa-rica', '16:00', 'germany')) +
-     createCard(
-        '02/12', 'sexta', 
+    createCard(
+        '02/12', 'sexta',
         createGame('south-korea', '12:00', 'portugal') +
         createGame('ghana', '12:00', 'uruguay') +
         createGame('serbia', '16:00', 'switzerland') +
