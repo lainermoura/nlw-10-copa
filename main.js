fcb5127a3814831a2f4b80d217b130e4714b5ab4
Laine Moura <lainermoura@gmail.com>
2022-11-13 15:22:54 -0300

    games: 11/21

--- main.js
+++ main.js
@@ -23,8 +23,12 @@ document.querySelector('#cards').innerHTML =
     createCard(
         '20/11', 'domingo', createGame('qatar', '13:00', 'ecuador')) +
     createCard(
+        '21/11', 'segunda',
+        createGame('england', '10:00', 'iran') +
+        createGame('senegal', '13:00', 'netherlands') +
+        createGame('usa', '16:00', 'wales')) +
+    createCard(
         '24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
-
     createCard(
         '28/11', 'segunda',
         createGame('brazil', '13:00', 'switzerland') +
